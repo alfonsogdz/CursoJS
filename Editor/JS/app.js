3a0ec004740262f730eddd9b49bb0d5bc16d57d2
Alfonso <alfonsogdz760@gmail.com>
2021-04-08 11:09:56 -0500

    Nuevo proyecto editor

---
+++ Editor/JS/app.js
@@ -0,0 +1,9 @@
+const text = document.querySelector("#text");
+const view = document.querySelector("#view")
+
+const update = () =>{
+
+    view.srcdoc = text.value;
+    console.log(text.value);
+  
+}
