adcc2e716eb796f626a9f5910726345acc48af6c
Alfonso <alfonsogdz760@gmail.com>
2021-04-08 10:17:25 -0500

    Se agregaron estilos al reloj

--- Reloj/JS/app.js
+++ Reloj/JS/app.js
@@ -1,5 +1,31 @@
 const getHora = () =>{
-    const fecha = new Date()
-    console.log(fecha) 
+    const fecha = new Date();
 
-}
+
+    const tiempo = {
+        hora: fecha.getHours(),
+        minuto: fecha.getMinutes(),
+        segundo: fecha.getSeconds(),
+    };
+
+    if(tiempo.hora >= 8 && tiempo.minuto >= 1 && tiempo.hora < 12){
+        document.querySelector("#contSaludo").innerHTML= "Buenos Dias";
+    }
+    if(tiempo.hora >= 12 && tiempo.minuto >= 1 && tiempo.hora < 19){
+        document.querySelector("#contSaludo").innerHTML= "Buenas Tardes";
+    }
+    if(tiempo.hora >= 19 && tiempo.minuto >= 1){
+        document.querySelector("#contSaludo").innerHTML= "Buenas Noches";
+    }
+    const ahora = `${tiempo.hora} : ${tiempo.minuto} : ${tiempo.segundo}`;
+
+    //console.log(ahora)
+    document.querySelector(".reloj").innerHTML = ahora; 
+
+
+};
+///getHora();
+
+
+
+setInterval(getHora, 1000);
